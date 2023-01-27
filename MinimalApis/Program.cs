ca10e30fc11a8e66ac9c7f7c02c0f070a0399b1c
AEX\AlexV <alexv@automationexchange.co.za>
2023-01-27 08:27:12 +0200

    add swagger

--- MinimalApis/Program.cs
+++ MinimalApis/Program.cs
@@ -7,7 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddEndpointDefinitions(typeof(IEndpointDefinition));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app
     .UseEndpointDefinitions()
